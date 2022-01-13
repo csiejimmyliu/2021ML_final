35f49a29dd0c71287bdc733d9204bf569b6a96a4
HONG-YING KE <laxingyang@cml24.csie.ntu.edu.tw>
2022-01-13 21:02:57 +0800

    tuning 6 step(not modified)

--- modeling.py
+++ modeling.py
@@ -19,25 +19,29 @@ from xgboost.sklearn import XGBClassifier
 import matplotlib.pyplot as plt
 %matplotlib inline
 from matplotlib.pyplot import rcParams
+
 rcParams['figure.figsize'] = 12, 4
 
 APPLY_NORMALIZATION = True
+#%%
+import os 
+os.getcwd()
 
 #%%
 if (APPLY_NORMALIZATION):
-    train_data = pd.read_csv('./preprocessed_data/train_data_normalized.csv')
-    train_data = train_data.loc[train_data['Churn Category'] != -1]
+    train = pd.read_csv('./preprocessed_data/train_normalized.csv')
+    train = train.loc[train['Churn Category'] != -1]
     target = 'Churn Category'
     IDcol = 'Customer ID'
     test_data = pd.read_csv('./preprocessed_data/test_data_normalized.csv')
 else:
-    train_data = pd.read_csv('./preprocessed_data/train_data.csv')
-    train_data = train_data.loc[train_data['Churn Category'] != -1]
+    train = pd.read_csv('./preprocessed_data/train.csv')
+    train = train.loc[train['Churn Category'] != -1]
     target = 'Churn Category'
     IDcol = 'Customer ID'
     test_data = pd.read_csv('./preprocessed_data/test_data.csv')
 
-NUM_CLASS = len(train_data[target].unique())
+NUM_CLASS = len(train[target].unique())
 
 #%%
 def modelfit(alg, dtrain, predictors,useTrainCV=True, cv_folds=5, early_stopping_rounds=50):
@@ -59,7 +63,7 @@ def modelfit(alg, dtrain, predictors,useTrainCV=True, cv_folds=5, early_stopping
     #Print model report:
     print("\nModel Report")
     print("Accuracy : %.4g" % metrics.accuracy_score(dtrain[target].values, dtrain_predictions))
-    # print("AUC Score (Train): %f" % metrics.roc_auc_score(dtrain[target], dtrain_predprob))
+    # print("AUC Score (Train): %f" % metrics.accuracy_score(dtrain[target], dtrain_predprob))
                     
     feat_imp = pd.Series(alg.get_booster().get_fscore()).sort_values(ascending=False)
     feat_imp.plot(kind='bar', title='Feature Importances')
@@ -67,7 +71,7 @@ def modelfit(alg, dtrain, predictors,useTrainCV=True, cv_folds=5, early_stopping
 
 #%%
 #Choose all predictors except target & IDcols
-predictors = [x for x in train_data.columns if x not in [target, IDcol]]
+predictors = [x for x in train.columns if x not in [target, IDcol]]
 xgb1 = XGBClassifier(
     learning_rate =0.1,
     n_estimators=1000,
@@ -82,23 +86,12 @@ xgb1 = XGBClassifier(
     num_class=NUM_CLASS,
     seed=1126
 )
-modelfit(xgb1, train_data, predictors)
+modelfit(xgb1, train, predictors)
 
+
+'''
 #%%
-xgb2 = XGBClassifier(
-    learning_rate =0.1,
-    n_estimators=1000,
-    max_depth=5,
-    min_child_weight=1,
-    gamma=0,
-    subsample=0.8,
-    colsample_bytree=0.8,
-    objective= 'multi:softprob',
-    nthread=4,
-    scale_pos_weight=1,
-    num_class=NUM_CLASS,
-    seed=1126
-)
+
 param_test1 = {
  'max_depth':range(3,10,2),
  'min_child_weight':range(1,6,2)
@@ -106,8 +99,175 @@ param_test1 = {
 gsearch1 = GridSearchCV(
     estimator=xgb2,param_grid=param_test1,scoring='accuracy',n_jobs=4,cv=5
 )
-gsearch1.fit(train_data[predictors],train_data[target])
+gsearch1.fit(train[predictors],train[target])
+gsearch1.cv_results_, gsearch1.best_params_, gsearch1.best_score_
+'''
+
+
+
+#step2
+
+# %%
+
+param_test1 = {
+ 'max_depth':range(3,10,2),
+ 'min_child_weight':range(1,6,2)
+}
+gsearch1 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=140, max_depth=5,
+ min_child_weight=1, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1, seed=27), 
+ param_grid = param_test1, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch1.fit(train[predictors],train[target])
 gsearch1.cv_results_, gsearch1.best_params_, gsearch1.best_score_
 
 # %%
-gsearch1.cv_results_
+
+
+# [-1,+1] of the param_test1 result
+param_test2 = {
+ 'max_depth':[4,5,6],
+ 'min_child_weight':[4,5,6]
+}
+gsearch2 = GridSearchCV(estimator = XGBClassifier( learning_rate=0.1, n_estimators=140, max_depth=5,
+ min_child_weight=2, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test2, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch2.fit(train[predictors],train[target])
+gsearch2.cv_results_, gsearch2.best_params_, gsearch2.best_score_
+
+
+# %%
+# try the bigger 4 number, if the param_test2 value is the max
+param_test2b = {
+ 'min_child_weight':[6,8,10,12]
+}
+gsearch2b = GridSearchCV(estimator = XGBClassifier( learning_rate=0.1, n_estimators=140, max_depth=4,
+ min_child_weight=2, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test2b, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch2b.fit(train[predictors],train[target])
+
+# %%
+modelfit(gsearch2.best_estimator_, train, predictors)
+gsearch2b.cv_results_, gsearch2b.best_params_, gsearch2b.best_score_
+
+
+
+# step 3
+#%%
+
+param_test3 = {
+ 'gamma':[i/10.0 for i in range(0,5)]
+}
+gsearch3 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=140, max_depth=4,
+ min_child_weight=6, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test3, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch3.fit(train[predictors],train[target])
+gsearch3.cv_results_, gsearch3.best_params_, gsearch3.best_score_
+
+
+#%%
+xgb2 = XGBClassifier(
+ learning_rate =0.1,
+ n_estimators=1000,
+ max_depth=4,
+ min_child_weight=6,
+ gamma=0,
+ subsample=0.8,
+ colsample_bytree=0.8,
+ objective= 'multi:softprob',
+ nthread=4,
+ scale_pos_weight=1,
+ seed=27)
+modelfit(xgb2, train, predictors)
+
+
+
+#step 4
+
+# %%
+param_test4 = {
+ 'subsample':[i/10.0 for i in range(6,10)],
+ 'colsample_bytree':[i/10.0 for i in range(6,10)]
+}
+gsearch4 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test4, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch4.fit(train[predictors],train[target])
+gsearch4.cv_results_, gsearch4.best_params_, gsearch4.best_score_
+
+
+#%%
+param_test5 = {
+ 'subsample':[i/100.0 for i in range(75,90,5)],
+ 'colsample_bytree':[i/100.0 for i in range(75,90,5)]
+}
+gsearch5 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test5, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch5.fit(train[predictors],train[target])
+
+
+#step5
+
+#%%
+param_test6 = {
+ 'reg_alpha':[1e-5, 1e-2, 0.1, 1, 100]
+}
+gsearch6 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0.1, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test6, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch6.fit(train[predictors],train[target])
+gsearch6.cv_results_, gsearch6.best_params_, gsearch6.best_score_
+
+
+#%%
+param_test7 = {
+ 'reg_alpha':[0, 0.001, 0.005, 0.01, 0.05]
+}
+gsearch7 = GridSearchCV(estimator = XGBClassifier( learning_rate =0.1, n_estimators=177, max_depth=4,
+ min_child_weight=6, gamma=0.1, subsample=0.8, colsample_bytree=0.8,
+ objective= 'multi:softprob', nthread=4, scale_pos_weight=1,seed=27), 
+ param_grid = param_test7, scoring='accuracy',n_jobs=4,iid=False, cv=5)
+gsearch7.fit(train[predictors],train[target])
+gsearch7.cv_results_, gsearch7.best_params_, gsearch7.best_score_
+
+#%%
+xgb3 = XGBClassifier(
+ learning_rate =0.1,
+ n_estimators=1000,
+ max_depth=4,
+ min_child_weight=6,
+ gamma=0,
+ subsample=0.8,
+ colsample_bytree=0.8,
+ reg_alpha=0.005,
+ objective= 'multi:softprob',
+ nthread=4,
+ scale_pos_weight=1,
+ seed=27)
+modelfit(xgb3, train, predictors)
+
+
+
+#step 6
+
+#%%
+xgb4 = XGBClassifier(
+ learning_rate =0.01,
+ n_estimators=5000,
+ max_depth=4,
+ min_child_weight=6,
+ gamma=0,
+ subsample=0.8,
+ colsample_bytree=0.8,
+ reg_alpha=0.005,
+ objective= 'multi:softprob',
+ nthread=4,
+ scale_pos_weight=1,
+ seed=27)
+modelfit(xgb4, train, predictors)
